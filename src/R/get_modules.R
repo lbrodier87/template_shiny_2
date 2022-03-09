6c4c2c3a2260eb401bb0ed566b7b5fdee1b8a191
Suvitha S <33218599+suvi-subra@users.noreply.github.com>
2022-03-09 17:38:29 +0100

    Added template

---
+++ src/R/get_modules.R
@@ -0,0 +1,28 @@
+#' Retrieves a list of all module aliases
+#'
+#' Module aliases retrieved via get_modules() are used to define module IDs in shiny::callModule(),
+#' and are also used as tabnames for the sidebar, and for the shiny UI label and id.
+#'
+#' @return list of strings containing module aliases
+#' @seealso \code{\link{app_ui}}, \code{\link{app_srv}}
+#' @export
+#'
+get_modules <- function(){
+  # a list of all module names
+  mod <- list(
+    ## Performance measures
+    recruit = "mod_recruit",
+    completeness = "mod_completeness",
+    consistency = "mod_consistency",
+    timeliness = "mod_timeliness",
+    queries = "mod_queries",
+    ## Study management
+    visits = "mod_visits",
+    completed = "mod_completed",
+    patient = "mod_patient",
+    safetymgm = "mod_safetymgm",
+    sae = "mod_sae",
+    ae = "mod_ae"
+  )
+  return(mod)
+}
---
+++ src/server.R
@@ -0,0 +1,22 @@
+#' The application server-side
+#' 
+#' @param input,output,session Internal parameters for {shiny}. 
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+
+library(shiny)
+
+app_server <- function(input, output, session ) {
+  ## Get all module names
+  mod <- get_modules()
+  
+  ## To get reactive data
+  rx.data <- get_reactive_data(input = input)
+  data <- get_data()
+  
+  ## Recruitment tab
+  callModule(mod_recruitment_server, mod$recruit, data.randomized = rx.data$rx_random)
+}
+
+
