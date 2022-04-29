70ec0f12df91eb630c91706ab8ba096b7d0f70ad
Laurent Brodier <lbrodier@bluewin.ch>
2022-04-29 17:37:37 +0200

    First draft of SAE module

--- R/get_data.R
+++ R/get_data.R
@@ -33,7 +33,21 @@ get_data <- function(){
                           long = c(6.5848, 8.9857, 8.9632, 7.4688, 10.2411),
                           lat = c(46.5980, 46.0868, 47.1502, 47.3604, 46.6630),
                           monthly = c(2,1,2,1,5))
-
+  
+  sae <- data.frame(pat_id = sample(randomized$pat_id, 50, replace = T),
+                    sae_date = sample(seq(as.Date('2017/12/01'), as.Date('2022/03/01'), by="day"), 50), 
+                    severity_level = sample(c("Mild", "Moderate", "Severe"), 50, replace = T), 
+                    causality = sample(c("Certain", "Probable", "Possible", "Unlikely", "Not related", "Not assessable"), 50, replace = T), 
+                    expectedness = sample(c("Expected", "Unexpcted"), 50, replace = T), 
+                    outcome = sample(c("Resolved without sequelae", "Resolved with sequelae", "Ongoing", "Death", "Unknown", "Other"), 50, replace = T), 
+                    death = sample(c("Yes", "No"), 50, T), 
+                    life_threatening = sample(c("Yes", "No"), 50, T),
+                    persistant_disability = sample(c("Yes", "No"), 50, T),
+                    hospitalization = sample(c("Yes", "No"), 50, T),
+                    congenital_anomyla_birth_defect = sample(c("Yes", "No"), 50, T))
+  sae$centre.short <- sapply(sae$pat_id, function(x){randomized$centre.short[randomized$pat_id == x]})
+  
+  
   #######################################################################################################################
   ###                                                     SAVE DATA                                                   ###
   #######################################################################################################################
@@ -41,7 +55,8 @@ get_data <- function(){
   data <- list(
     data.extraction.date = data.extraction.date,
     randomized = randomized,
-    locations = locations
+    locations = locations,
+    sae = sae
   )
 
   return(data)
--- R/mod_sae.R
+++ R/mod_sae.R
@@ -4,6 +4,7 @@
 #' @rdname mod_sae
 #' @param id standard shiny id argument
 #' @param label standard shiny label argument
+#' @author Laurent Brodier
 mod_sae_ui <- function(id, label){
   ns <- NS(id)
   tabItem(tabName = label,
@@ -12,11 +13,28 @@ mod_sae_ui <- function(id, label){
                    title = "",
                    id = "tabset2",
                    height = "450px",
-                   tabPanel("Serious Adverse Events",
-                            "This module is under development",
-                            # height = "400"
+                   tabPanel("SAE List",
+                            "SAE List: ",
+                            tableOutput(ns("sae_table_1"))
+                            ), 
+                   tabPanel("SAE Plot", 
+                            "SAE plot overtime:",
+                            plotlyOutput(ns("sae_plot_1"))
+                            ),
+                   tabPanel("SAE Plot by center",
+                            "SAE plot overtime by center:",
+                           plotlyOutput(ns("sae_plot_2"))
+                           ), 
+                   tabPanel("SAE count by factor", 
+                            "SAE count by center and by characteristics: ", 
+                            selectInput(ns("sae_fact_sel"), 
+                                        choices = c("None", "Severity level", "Outcome", "Causality", 
+                                                    "Expectedness", "Death", "Life threatening", "Persistant disability", 
+                                                    "Hospitalization", "Congenital anomaly / birth defect"), 
+                                        label = "Select SAE characteristics to display: "),
+                            plotlyOutput(ns("sae_plot_3"))
                             )
-                   )
+              )
             )
           )
 }
@@ -26,10 +44,61 @@ mod_sae_ui <- function(id, label){
 #' @param output standard shiny output argument
 #' @param session standard shiny session argument
 #' @param data data for use in calculations
-mod_sae_server <- function(input, output, session, data){
+mod_sae_server <- function(input, output, session, data.sae){
 
   ns <- session$ns
+  
+  #constants - to map to your dataset variable names
+  record_id <- "pat_id"
+  center <- "centre.short"
+  sae_date <- "sae_date"
 
-  # message(dat())
+  output$sae_plot_1 <- renderPlotly({
+    d <- data.sae[order(data.sae$sae_date),]
+    d$cumul <- seq_along(d$sae_date)
+    a <- aggregate(data = d, cumul ~ sae_date, max)
+    p <- ggplot(data = NULL, aes(x=d[,sae_date], y=d$cumul)) + geom_line() + 
+      theme_bw() + labs(x="date", y="SAE cumulative number")
+    plotly::ggplotly(p)
+  })
+  
+  output$sae_plot_2 <- renderPlotly({
+    d <- data.sae[order(data.sae$sae_date),]
+    d$cumul <- seq_along(d$sae_date)
+    a <- aggregate(data = d, cumul ~ sae_date, max)
+    p <- ggplot(data = NULL, aes(x=d[,sae_date], y=d$cumul, color=d[,center])) + geom_line() + 
+      theme_bw() + labs(x="date", y="SAE cumulative number", color="Center")
+    plotly::ggplotly(p)
+  })
+  
+  output$sae_plot_3 <- renderPlotly({
+    
+    f <- input$sae_fact_sel
+    if(f=="None") f <- NULL 
+    else if(f=="Severity level") f <- "severity_level"
+    else if(f=="Outcome") f <- "outcome"
+    else if(f=="Causality") f <- "causality"
+    else if(f=="Expectedness") f <- "expectedness"
+    else if(f=="Expectedness") f <- "expectedness"
+    else if(f=="Death") f <- "death"
+    else if(f=="Life threatening") f <- "life_threatening"
+    else if(f=="Persistant disability") f <- "persistant_disability"
+    else if(f=="Hospitalization") f <- "hospitalization"
+    else if(f=="Congenital anomaly / birth defect") f <- "congenital_anomyla_birth_defect"
+    
+    if(!is.null(f)){
+      a <- aggregate(data = data.sae, as.formula(paste0("pat_id ~ centre.short*", f)), length)
+      p <- ggplot(data = a, aes(x=centre.short, y=pat_id, fill=a[,f])) + 
+        geom_col() + theme_bw() + labs(x = "center name", y="SAE count", fill=input$sae_fact_sel)
+    }else{
+      a <- aggregate(data = data.sae, pat_id ~ centre.short, length)
+      p <- ggplot(data = a, aes(x=centre.short, y=pat_id)) + 
+        geom_col() + theme_bw() + labs(x = "center name", y="SAE count")
+    }
+  })
+  
+  output$sae_table_1 <- renderTable({
+    data.sae
+  })
 
 }
