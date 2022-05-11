63a8fd8141d69e5d9570a3c32ec42251e105766b
suvi-subra <33218599+suvi-subra@users.noreply.github.com>
2022-05-11 14:32:15 +0200

    changes for Lucien

--- R/get_reactive_data.R
+++ R/get_reactive_data.R
@@ -17,12 +17,23 @@ get_reactive_data <- function(input){
     } else{
       filter(data$randomized, rando_date.date >= input$period[1] & rando_date.date <= input$period[2])
     })
+  
+  
+  # missing_period <- reactive(
+  #   
+  #   if(input$center != "All"){
+  #     filter(data$missing, centre.short == input$center & rando_date.date >= input$period[1] & rando_date.date <= input$period[2])
+  #     
+  #   } else{
+  #     filter(data$missing, rando_date.date >= input$period[1] & rando_date.date <= input$period[2])
+  #   })
 
 
   ## Save reactive datafranes into list
 
   reactive_data <- list(
     rx_random = random_period
+    # , rx_missing = missing_period
   )
 
   return(reactive_data)
