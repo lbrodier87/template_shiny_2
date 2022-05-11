395a8fc3d8a2cc87f5be44fa84d7331966387f5c
Laurent Brodier <lbrodier@bluewin.ch>
2022-05-11 16:59:24 +0200

    Update mod_sae.R

--- R/mod_sae.R
+++ R/mod_sae.R
@@ -13,10 +13,7 @@ mod_sae_ui <- function(id, label){
                    title = "",
                    id = "tabset2",
                    height = "450px",
-                   tabPanel("SAE List",
-                            "SAE List: ",
-                            tableOutput(ns("sae_table_1"))
-                            ), 
+                   
                    tabPanel("SAE Plot", 
                             "SAE plot overtime:",
                             plotlyOutput(ns("sae_plot_1"))
@@ -35,7 +32,11 @@ mod_sae_ui <- function(id, label){
                             #"add filters with code generated choices",
                             #Check order of levels on selection! 
                             plotlyOutput(ns("sae_plot_3"))
-                            )
+                            ), 
+                   tabPanel("SAE List",
+                            "SAE List: ",
+                            tableOutput(ns("sae_table_1"))
+                   )
               )
             )
           )
@@ -55,32 +56,34 @@ mod_sae_server <- function(input, output, session, data.sae){
   center <- "centre.short"
   sae_date <- "sae_date"
 
+  #plot of cumulative SAE number (all centers)
   output$sae_plot_1 <- renderPlotly({
-    d <- data.sae[order(data.sae$sae_date),]
-    d$cumul <- seq_along(d$sae_date)
-    a <- aggregate(data = d, cumul ~ sae_date, max)
+    d <- data.sae[order(data.sae[,sae_date]),] #order the SAE by date
+    d$cumul <- seq_along(d[,sae_date]) #get cumulative count of SAE
+    a <- aggregate(data = d, as.formula(paste0("cumul ~ ", sae_date)), max) #aggregate max (to acount for <1 SAE on same date)
     p <- ggplot(data = NULL, aes(x=d[,sae_date], y=d$cumul)) + geom_line() + 
       theme_bw() + labs(x="date", y="SAE cumulative number")
     plotly::ggplotly(p)
   })
   
+  #plot of cumulative SAE number (color by center)
   output$sae_plot_2 <- renderPlotly({
-    d <- data.sae[order(data.sae$sae_date),]
-    d$cumul <- seq_along(d$sae_date)
-    a <- aggregate(data = d, cumul ~ sae_date, max)
+    d <- data.sae[order(data.sae[,sae_date]),] #order the SAE by date
+    d$cumul <- seq_along(d[,sae_date]) #get cumulative count of SAE
+    a <- aggregate(data = d, as.formula(paste0("cumul ~ ", sae_date)), max) #aggregate max (to acount for <1 SAE on same date)
     p <- ggplot(data = NULL, aes(x=d[,sae_date], y=d$cumul, color=d[,center])) + geom_line() + 
       theme_bw() + labs(x="date", y="SAE cumulative number", color="Center")
     plotly::ggplotly(p)
   })
   
+  #plot SAE bar, color by category
   output$sae_plot_3 <- renderPlotly({
-    
+    #get input: what factor to use as color? + map to df col name
     f <- input$sae_fact_sel
     if(f=="None") f <- NULL 
     else if(f=="Severity level") f <- "severity_level"
     else if(f=="Outcome") f <- "outcome"
     else if(f=="Causality") f <- "causality"
-    else if(f=="Expectedness") f <- "expectedness"
     else if(f=="Expectedness") f <- "expectedness"
     else if(f=="Death") f <- "death"
     else if(f=="Life threatening") f <- "life_threatening"
@@ -89,12 +92,12 @@ mod_sae_server <- function(input, output, session, data.sae){
     else if(f=="Congenital anomaly / birth defect") f <- "congenital_anomyla_birth_defect"
     
     if(!is.null(f)){
-      a <- aggregate(data = data.sae, as.formula(paste0("pat_id ~ centre.short*", f)), length)
-      p <- ggplot(data = a, aes(x=centre.short, y=pat_id, fill=a[,f])) + 
-        geom_col() + theme_bw() + labs(x = "center name", y="SAE count", fill=input$sae_fact_sel)
+      a <- aggregate(data = data.sae, as.formula(paste0(record_id, " ~ ", center, "*", f)), length)
+      p <- ggplot(data = NULL, aes(x=a[,center], y=a[,record_id], fill=a[,f])) + 
+        geom_col() + theme_bw() + labs(x = "Center name", y="SAE count", fill=input$sae_fact_sel)
     }else{
-      a <- aggregate(data = data.sae, pat_id ~ centre.short, length)
-      p <- ggplot(data = a, aes(x=centre.short, y=pat_id)) + 
+      a <- aggregate(data = data.sae, as.formula(paste0(record_id, " ~ ", center)), length)
+      p <- ggplot(data = NULL, aes(x=a[,center], y=a[,record_id])) + 
         geom_col() + theme_bw() + labs(x = "center name", y="SAE count")
     }
   })
