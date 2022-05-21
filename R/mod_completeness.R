84c34451d3bf41187f42c2e7ac5a47a266ec7014
gygl <lucien.gyger@gmail.com>
2022-05-21 19:32:36 +0200

    add 4 plots to module completeness
    - plot overview dataset
    - plot variable missingness
    - plot case missingness
    - plot pattern of missingness using upset matrix design (form UpSetR::upset but implemented in naniar::gg_miss_upset) [Note: visualzation is bad if too much different combination of missingness]

--- R/mod_completeness.R
+++ R/mod_completeness.R
@@ -1,4 +1,6 @@
 #' Completeness
+#' 
+#' @import naniar
 #'
 #' In development
 #' @rdname mod_completeness
@@ -13,14 +15,26 @@ mod_completeness_ui <- function(id, label){
                    title = "",
                    id = "tabset2",
                    height = "850px",
-                   selected = "Completeness",
-                   tabPanel("Completeness",
-                            # height = "400"
-                            plotOutput(ns('plot'), height = "750")
-                            )
+                   selected = "Completeness"
+                   , tabPanel("Overview form",
+                            height = "400",
+                            plotlyOutput(ns('vis_miss'), height = "750")
+                   )
+                   , tabPanel("Variable missingness",
+                              height = "400",
+                              plotlyOutput(ns('var_miss'), height = "750")
+                   )
+                   , tabPanel("Case missingness",
+                              height = "400",
+                              plotlyOutput(ns('case_miss'), height = "750")
+                   )
+                   , tabPanel("Missingness pattern",
+                              height = "300",
+                              plotOutput(ns('miss_pattern'), height = "750")
                    )
             )
           )
+  )
 }
 
 #' @rdname mod_completeness
@@ -28,17 +42,24 @@ mod_completeness_ui <- function(id, label){
 #' @param output standard shiny output argument
 #' @param session standard shiny session argument
 #' @param data data for use in calculations
-mod_completeness_server <- function(input, output, session, data){
-
+mod_completeness_server <- function(input, output, session, data_form){
+  
   ns <- session$ns
   
-  output$plot <- renderPlot({
-  
-    ggplot(data = data, aes(x = age, y = weight)) + geom_point()
-
+  output$vis_miss <- renderPlotly({
+    data_form() %>% vis_miss %>% ggplotly
   })
-
-
-  # message(dat())
+  
+  output$var_miss <- renderPlotly({
+    data_form() %>% gg_miss_var(show_pct = TRUE) %>% ggplotly
+  })
+  
+  output$case_miss <- renderPlotly({
+    data_form() %>% gg_miss_case(show_pct = TRUE) %>% ggplotly
+  })
+  
+  output$miss_pattern <- renderPlot({
+    data_form() %>% gg_miss_upset(text.scale = 3, nsets = ncol(data_form()))
+  })
 
 }
